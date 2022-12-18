ebe5dabdb6a6bbf6f88514fd24953094d1e9b346
Riazkareem <riazahmadgithub@gmail.com>
2022-12-18 17:34:34 +0500

    12/18/22

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Navbar() {
   const router = useRouter();
+  // it put active class to current page or nav element
   const isActive = (r) => {
     if (r === router.pathname) {
       return " active text-white bg-[#2557D6] px-2 py-1";
@@ -12,6 +13,15 @@ function Navbar() {
       return "";
     }
   };
+
+  // it hides the dropdown menu
+  const [show, setShow] = useState(true);
+  function closeModel() {
+    setShow(false);
+    setTimeout(() => {
+      setShow(true);
+    }, 1000);
+  }
 
   return (
     <section>
@@ -110,116 +120,118 @@ function Navbar() {
                       ></path>
                     </svg>
                   </button>
-                  <div
-                    id="mega-menu-full-cta-dropdown"
-                    className="hidden peer-hover:flex hover:block absolute top-30 right-12 mt-0 bg-white border-gray-200 drop-shadow-lg border-y dark:bg-gray-800 dark:border-gray-600 overflow-y-auto h-1/2 mobile:right-3 mobile:left-3 mobile:h-96 mobile:z-50"
-                  >
-                    <div className="grid py-5 px-4 mx-auto max-w-screen-xl text-sm text-gray-500 dark:text-gray-400 md:grid-cols-3 md:px-6">
-                      <ul className="space-y-4 sm:mb-4 md:mb-0">
-                        <h3 className="font-bold text-xl text-gray-900 text-bold mb-2">
-                          Main Category 1
-                        </h3>
-                        <li>
-                          <Link
-                            href={"/"}
-                            className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
+                  {show ? (
+                    <div
+                      id="mega-menu-full-cta-dropdown"
+                      className="hidden peer-hover:flex hover:block absolute top-30 right-12 mt-0 bg-white border-gray-200 drop-shadow-lg border-y dark:bg-gray-800 dark:border-gray-600 overflow-y-auto h-1/2 mobile:right-3 mobile:left-3 mobile:h-96 mobile:z-50"
+                    >
+                      <div className="grid py-5 px-4 mx-auto max-w-screen-xl text-sm text-gray-500 dark:text-gray-400 md:grid-cols-3 md:px-6">
+                        <ul className="space-y-4 sm:mb-4 md:mb-0">
+                          <h3 className="font-bold text-xl text-gray-900 text-bold mb-2">
+                            Main Category 1
+                          </h3>
+                          <li onClick={closeModel}>
+                            <Link
+                              href={"/"}
+                              className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
+                            >
+                              category name
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              href={"/"}
+                              className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
+                            >
+                              category name
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              href={"/"}
+                              className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
+                            >
+                              category name
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              href={"/"}
+                              className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
+                            >
+                              category name
+                            </Link>
+                          </li>
+                        </ul>
+                        <ul className="mb-4 space-y-4 md:mb-0 sm:block">
+                          <h3 className="font-bold text-xl text-gray-900 text-bold mb-2">
+                            Main Category 2
+                          </h3>
+                          <li>
+                            <Link
+                              href={"/"}
+                              className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
+                            >
+                              category name
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              href={"/"}
+                              className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
+                            >
+                              category name
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              href={"/"}
+                              className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
+                            >
+                              category name
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              href={"/"}
+                              className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
+                            >
+                              category name
+                            </Link>
+                          </li>
+                        </ul>
+                        <div className="mt-4 md:mt-0">
+                          <h3 className="font-bold text-xl text-gray-900 text-bold mb-2">
+                            Main Category 3
+                          </h3>
+                          <p className="mb-2 font-light text-gray-500 dark:text-gray-400">
+                            At Flowbite, we have a portfolio of brands that
+                            cater to a variety of preferences.
+                          </p>
+                          <a
+                            href="#"
+                            className="inline-flex items-center text-sm font-medium text-blue-600 hover:underline hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-700"
                           >
-                            category name
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href={"/"}
-                            className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
-                          >
-                            category name
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href={"/"}
-                            className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
-                          >
-                            category name
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href={"/"}
-                            className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
-                          >
-                            category name
-                          </Link>
-                        </li>
-                      </ul>
-                      <ul className="mb-4 space-y-4 md:mb-0 sm:block">
-                        <h3 className="font-bold text-xl text-gray-900 text-bold mb-2">
-                          Main Category 2
-                        </h3>
-                        <li>
-                          <Link
-                            href={"/"}
-                            className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
-                          >
-                            category name
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href={"/"}
-                            className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
-                          >
-                            category name
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href={"/"}
-                            className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
-                          >
-                            category name
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href={"/"}
-                            className="text-base capitalize hover:bg-[#2557D6] hover:px-2 hover:py-1 hover:text-white"
-                          >
-                            category name
-                          </Link>
-                        </li>
-                      </ul>
-                      <div className="mt-4 md:mt-0">
-                        <h3 className="font-bold text-xl text-gray-900 text-bold mb-2">
-                          Main Category 3
-                        </h3>
-                        <p className="mb-2 font-light text-gray-500 dark:text-gray-400">
-                          At Flowbite, we have a portfolio of brands that cater
-                          to a variety of preferences.
-                        </p>
-                        <a
-                          href="#"
-                          className="inline-flex items-center text-sm font-medium text-blue-600 hover:underline hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-700"
-                        >
-                          Explore our brands
-                          <span className="sr-only">Explore our brands </span>
-                          <svg
-                            className="ml-1 w-4 h-4"
-                            aria-hidden="true"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                              clipRule="evenodd"
-                            ></path>
-                          </svg>
-                        </a>
+                            Explore our brands
+                            <span className="sr-only">Explore our brands </span>
+                            <svg
+                              className="ml-1 w-4 h-4"
+                              aria-hidden="true"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                                clipRule="evenodd"
+                              ></path>
+                            </svg>
+                          </a>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  ) : null}
                 </li>
                 <style jsx>
                   {`
